cf4afca1a57b8184925bfecb7653d99daf09cb35
limaohuan <2112176929@qq.com>
2025-04-28 10:44:44 +0800

    make Aura orient rotation to movement

--- Source/Aura/Private/Character/AuraCharacter.cpp
+++ Source/Aura/Private/Character/AuraCharacter.cpp
@@ -2,4 +2,20 @@
 
 
 #include "Character/AuraCharacter.h"
+#include "GameFramework/CharacterMovementComponent.h"
 
+AAuraCharacter::AAuraCharacter()
+{
+	// 设置角色移动方向自动旋转到移动方向
+	GetCharacterMovement()->bOrientRotationToMovement = true;
+	GetCharacterMovement()->RotationRate = FRotator(0.f, 400.f, 0.f);
+	// 将角色移动约束在平面上 (防止Z轴移动)
+	GetCharacterMovement()->bConstrainToPlane = true;
+	// 游戏开始时将角色对齐到平面
+	GetCharacterMovement()->bSnapToPlaneAtStart = true;
+
+	// 禁用控制器旋转对角色旋转的影响
+	bUseControllerRotationPitch = false;
+	bUseControllerRotationRoll = false;
+	bUseControllerRotationYaw = false;
+}
