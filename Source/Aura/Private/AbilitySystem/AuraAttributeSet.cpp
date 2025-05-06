daa3eb0b3ecfcbf39725ee287bf1b614438d5c21
limaohuan <2112176929@qq.com>
2025-05-06 20:21:08 +0800

    add attribute_accessor for 4 attributes and init them

--- Source/Aura/Private/AbilitySystem/AuraAttributeSet.cpp
+++ Source/Aura/Private/AbilitySystem/AuraAttributeSet.cpp
@@ -2,12 +2,14 @@
 
 
 #include "AbilitySystem/AuraAttributeSet.h"
-#include "AbilitySystemComponent.h"
 #include "Net/UnrealNetwork.h"
 
 UAuraAttributeSet::UAuraAttributeSet()
 {
-
+	InitHealth(100.f);
+	InitMaxHealth(200.f);
+	InitMana(80.f);
+	InitMaxMana(160.f);
 }
 
 void UAuraAttributeSet::GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const
